4c11d99938b7092abe805b0dc75a91b8bc9492a9
SrishtiSk <48864294+SrishtiSk@users.noreply.github.com>
2021-05-04 12:09:07 +0530

    porporty list componet and housing service

--- src/app/Property/property-card/property-card.component.ts
+++ src/app/Property/property-card/property-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-property-card',
@@ -7,13 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PropertyCardComponent implements OnInit {
 
-  Property:any={
-    "Id":1,
-    "Name": "Bly Mannor",
-    "Type":"Mannor",
-    "Price":12000
-
-  }
+@Input() propertycard:any
 
   constructor() { }
 

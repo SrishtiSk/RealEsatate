5ecd43b01d112ab02a8f817ee925fec4debab32a
SrishtiSk <48864294+SrishtiSk@users.noreply.github.com>
2021-05-07 14:48:42 +0530

    forms for add property and user registration

--- src/app/Property/add-property/add-property.component.ts
+++ src/app/Property/add-property/add-property.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
@@ -6,15 +7,29 @@ import { Router } from '@angular/router';
   templateUrl: './add-property.component.html',
   styleUrls: ['./add-property.component.css']
 })
+
+
 export class AddPropertyComponent implements OnInit {
+
+  @ViewChild('Form') addPropertyForm : NgForm;
 
   constructor(private router: Router) { }
 
   ngOnInit(): void {
+   // this.addPropertyForm.controls['Name'].setValue('Default Value');
+    setTimeout(()=>{
+        this.addPropertyForm.controls['Name'].setValue('Default Value');
+    });
+
   }
 
   onBack(){
     this.router.navigate(['/']);
+  }
+
+  onSubmit(Form:NgForm){
+    console.log("Submitted");
+    console.log(this.addPropertyForm)
   }
 
 }

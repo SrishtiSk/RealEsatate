a775a840b7720859f6f2ab1031769edd46175b4f
SrishtiSk <48864294+SrishtiSk@users.noreply.github.com>
2021-05-26 15:33:47 +0530

    Finally fixed the gitignore for WEBAPI_CORE

--- WebApi_Code/Controllers/CityController.cs
+++ WebApi_Code/Controllers/CityController.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-//using WebApi Code.Models;
 
 namespace WebApi_Code.Controllers
 {

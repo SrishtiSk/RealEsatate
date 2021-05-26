452711720de8483b2d953a3ae499db8f8036a051
SrishtiSk <48864294+SrishtiSk@users.noreply.github.com>
2021-05-26 11:35:12 +0530

    the migration

---
+++ RealEsatate WebApi/Controllers/CityController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RealEsatate_WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CityController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<string> Get() 
+        {
+            return new string[] { "Atlanta, NewYork, Canada, Boston" };
+        }
+
+        [HttpGet("{id}")]
+        public  string  Get(int id)
+        {
+            return "Atlanta";
+        }
+
+    }
+}
---
+++ WebApi Code/Controllers/CityController.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+//using WebApi Code.Models;
+
+namespace WebApi_Code.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CityController : ControllerBase
+    {
+        public CityController()
+        {
+        }
+
+        [HttpGet("")]
+        public IEnumerable<string> GetString()
+        {
+            return new string[] {"Atlanta, New York, Canada, boston, Chicago"};
+        }
+
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "Atlanta";
+        }
+
+    }
+}
